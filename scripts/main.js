1473aadc8bd3338a1f5ea952bb4dbfae8707548c
QmelZ <qmelzthevoid@gmail.com>
2021-02-02 23:27:21 +0300

    h sldj

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ let strobe = extend(Block, "strobe-lights", {
 let effect = new Effect(Infinity, e => {
     Draw.z(Layer.weather);
     Draw.color(Color.red.cpy().shiftHue(Time.time * 8));
-    Fill.poly(e.x, e.y, 4, 99999, 0);
+    Fill.poly(Vars.player.x, Vars.player.y, 4, 99999, 0);
 });
 
 strobe.buildType = () => extend(Building, {
@@ -23,6 +23,7 @@ strobe.buildType = () => extend(Building, {
         music.play();
         music.setVolume(1);
         Vars.ui.hudGroup.clearChildren();
+        Core.graphics.setFullscreenMode(Core.graphics.getDisplayMode());
         effect.at(Vars.player.x, Vars.player.y);
         this.block.buildVisibility = BuildVisibility.hidden;
     }
